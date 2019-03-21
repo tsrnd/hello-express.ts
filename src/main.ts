1d48e660efc5bbb7cd3f182964b69f9b9b1f8468
at-namnguyen <nam.nguyen@asiantech.vn>
2019-03-21 11:22:37 +0700

    add express app

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,8 @@
-console.log('comming soon')
+import app from './app'
+import * as util from 'util'
+
+const port = process.env.APP_PORT
+
+app.listen(port, ()=> {
+    console.log(util.format('Server is running on port %d', port))
+})
