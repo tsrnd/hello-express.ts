6642152e435107d3612413daffd6904e7f77760a
at-namnguyen <nam.nguyen@asiantech.vn>
2019-03-21 11:58:13 +0700

    add tslint.json

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
-import app from './app'
-import * as util from 'util'
+import app from './app';
+import * as util from 'util';
 
-const port = process.env.APP_PORT
+const port = process.env.APP_PORT;
 
-app.listen(port, ()=> {
-    console.log(util.format('Server is running on port %d', port))
-})
+app.listen(port, () => {
+    console.log(util.format('Server is running on port %d', port));
+});
