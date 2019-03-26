1e524085bffc400de8b3ddd472e71c23da0c1090
at-nale <na.le@asiantech.vn>
2019-03-26 17:06:38 +0700

    example crud ts

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,6 +1,6 @@
 import * as mongoose from "mongoose";
 import { User } from "./../models/user";
-import { Request, Response } from "express";
+import { Request, Response, response } from "express";
 
 const user = mongoose.model("User", User);
 
@@ -13,12 +13,44 @@ export class UserController {
             }
             resp.render("index", {
                 users: result
-            }, (err) => {
-                if (err) {
-                    console.log(err);
-                    resp.status(500).end();
-                }
             });
         });
     }
+    public addUser(req: Request, resp:Response){
+        let newUser = new user(req.body);
+        newUser.save((err, user)=>{
+            if(err){
+                resp.status(500).send(err);
+            }    
+            resp.json(user);
+        })
+
+    }
+    public detail(req: Request, resp: Response){
+        user.findById(req.params.id, (err, user)=>{
+            if(err) { 
+                resp.send(err);
+            }
+            resp.json(user);
+        })
+    }
+    public edit(req: Request, resp: Response){
+        user.findOneAndUpdate({_id:req.params.id}, req.body, (err,user)=>{
+            if(err){
+                resp.send(err);
+            }
+            resp.json(user);
+        })
+    }
+    public delete(req: Request, resp: Response) {
+        user.remove({_id:req.params.id}, (err, user) => {
+            if(err){
+                resp.send(err);
+            }
+            resp.json({
+                message:"Deleted Successfully"
+            })
+        })
+    }
+
 }
--- src/routes/route.ts
+++ src/routes/route.ts
@@ -6,5 +6,9 @@ export class Routes {
     public userController: UserController = new UserController();
     public routes(app): void {
         app.get("/users", this.userController.get);
+        app.post("/users", this.userController.addUser);
+        app.get("/user/:id", this.userController.detail);
+        app.put("/user/:id", this.userController.edit);
+        app.delete("/user/:id", this.userController.delete);
     }
 }
