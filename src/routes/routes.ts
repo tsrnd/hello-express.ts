da8aba8120c32b124a9b4e668153ac0d12f2b0c0
at-hieuvo <hieu.vo@asiantech.vn>
2019-04-02 09:11:16 +0700

    socket io

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -2,11 +2,15 @@ import { Request, Response } from 'express';
 
 export class Routes {
     public routes(app): void {
+        // app.route('/')
+        //     .get((req: Request, res: Response) => {
+        //         res.status(200).send({
+        //             message: 'Hello nodejs team1'
+        //         });
+        //     });
         app.route('/')
             .get((req: Request, res: Response) => {
-                res.status(200).send({
-                    message: 'Hello nodejs team1'
-                });
+                res.sendFile(__dirname + '/index.html');
             });
     }
 }
